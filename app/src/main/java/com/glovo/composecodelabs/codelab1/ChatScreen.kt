12fe0c5bba2b61ca798da8390a92ce8e8557f157
kulbakan <natalyastel@gmail.com>
2025-06-17 15:26:44 +0200

    Add top bar shadow

--- app/src/main/java/com/glovo/composecodelabs/codelab1/ChatScreen.kt
+++ app/src/main/java/com/glovo/composecodelabs/codelab1/ChatScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,18 +22,21 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
 fun ChatScreen(vm: ChatViewModel) {
     val uiState by vm.uiState.collectAsStateWithLifecycle()
+    val bodyLazyListState = rememberLazyListState()
     println("ChatScreen recomposition")
     Scaffold(
         topBar = {
             println("Scaffold topBar recomposition")
-            ChatTopBar(uiState.messages)
+            ChatTopBar(uiState.messages, bodyLazyListState)
         },
         bottomBar = {
             println("Scaffold bottomBar recomposition")
@@ -39,18 +44,25 @@ fun ChatScreen(vm: ChatViewModel) {
         },
     ) { paddingValues ->
         println("Scaffold content recomposition")
-        Chat(uiState.messages, vm::onEdit, paddingValues)
+        Chat(uiState.messages, vm::onEdit, paddingValues, bodyLazyListState)
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ChatTopBar(messages: List<Message>) {
+fun ChatTopBar(messages: List<Message>, bodyLazyListState: LazyListState) {
     println("ChatTopBar recomposition")
-    TopAppBar(title = {
-        println("TopAppBar title recomposition")
-        Text("Messages ${messages.size}")
-    })
+    TopAppBar(
+        title = {
+            println("TopAppBar title recomposition")
+            Text("Messages ${messages.size}")
+        },
+        modifier = Modifier.applyIf(bodyLazyListState.isScrolled) {
+            Modifier
+                .zIndex(HEADER_Z_INDEX)
+                .shadow(HEADER_SHADOW)
+        }
+    )
 }
 
 @Composable
@@ -73,13 +85,19 @@ fun ChatBottomBar(messageInput: String, onInputChanged: (String) -> Unit, onSend
 }
 
 @Composable
-fun Chat(messages: List<Message>, onEdit: (Int) -> Unit, paddingValues: PaddingValues) {
+fun Chat(
+    messages: List<Message>,
+    onEdit: (Int) -> Unit,
+    paddingValues: PaddingValues,
+    bodyLazyListState: LazyListState
+) {
     println("Chat recomposition")
     LazyColumn(
         modifier = Modifier
             .padding(paddingValues)
             .fillMaxWidth()
-            .fillMaxHeight()
+            .fillMaxHeight(),
+        state = bodyLazyListState
     ) {
         println("LazyColumn recomposition")
         items(messages) { message ->
@@ -99,6 +117,20 @@ fun MessageItem(message: Message, onEdit: (Int) -> Unit) {
         Text(message.text, modifier = Modifier.padding(12.dp))
     }
 }
+
+val LazyListState.isScrolled: Boolean
+    get() = firstVisibleItemIndex > 0 || firstVisibleItemScrollOffset > 0
+
+const val HEADER_Z_INDEX = 10f
+val HEADER_SHADOW = 8.dp
+
+@Composable
+fun Modifier.applyIf(condition: Boolean, modifier: @Composable Modifier.() -> Modifier): Modifier =
+    if (condition) {
+        then(modifier(Modifier))
+    } else {
+        this
+    }
 
 @Preview
 @Composable
